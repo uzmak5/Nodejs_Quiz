32ddf30ac80d3ec80cfb202b03b007bfb1760c5c
Uzma Nareen <nareen.khan98@gmail.com>
2022-03-29 20:01:49 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "Which of the following command is used to start a REPL session?",
+    choice1: "$ node",
+    choice2: " $ node start",
+    choice3: "$ node repl",
+    choice4: "$ node console",
+    answer: 0
+},
+            {
+            question: " Which of the following module is not a built-in node module?",
+    choice1: "zlib",
+    choice2: "https",
+    choice3: "dgram",
+    choice4: "fsread",
+    answer: 3
+},
+            {
+            question: "Which of the following method of fs module is used to get file information?",
+    choice1: "fs.open(path, flags[, mode], callback)",
+    choice2: "fs.stat(path, callback)",
+    choice3: "fs.readFile(path, flags[, mode], callback)",
+    choice4: "None of the above.",
+    answer: 1
+},
+            {
+            question: "Which of the following shortcut command is used to kill a process in Node.js?",
+    choice1: "Ctrl + B",
+    choice2: "Ctrl + K",
+    choice3: "Ctrl + T",
+    choice4: "Ctrl + C",
+    answer: 3
+},
+            {
+            question: "How many Node object methods are available?",
+    choice1: "21",
+    choice2: "19",
+    choice3: "18",
+    choice4: "25",
+    answer: 2
+},
+            {
+            question: "Which of the following code print the platform of operating system?",
+    choice1: "console.log('platform : ' + os.platform);",
+    choice2: "console.log('platform : ' + os.platform());",
+    choice3: "console.log('platform : ' + os.getPlatform());",
+    choice4: "None of the above",
+    answer: 1
+},
+            {
+               question: "What is the default scope in the Node.js application?",
+    choice1: "Global",
+    choice2: "Local",
+    choice3: "Global Function",
+    choice4: " Local to object",
+    answer: 1
+},{
+               question: "Which of the following types of applications can be built using Node.js?",
+    choice1: "Web Application",
+    choice2: "Chat Application",
+    choice3: "RESTful Service",
+    choice4: "All of the above",
+    answer: 3
+},
+            {
+               question: "Which of the following is a GUI-based debugging tool for Node.js?",
+    choice1: "Node Inspector",
+    choice2: "REPL",
+    choice3: "Console",
+    choice4: "Core node debugger",
+    answer: 0
+},
+            {
+               question: "Which of the following method is used to compare the placement of two nodes in the DOM hierarchy (document)?",
+    choice1: "cloneNode()",
+    choice2: "compareDocumentPosition()",
+    choice3: "getFeature()",
+    choice4: " getUserData()",
+    answer: 1
+}];
